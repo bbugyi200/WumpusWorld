02c86e702e46c385781ee719aed9a96b49648c27
Bryan Bugyi <bryan_bugyi@mymail.rcbc.edu>
2017-03-03 13:24:44 -0500

    Comment on tests.kbank

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ os.system('clear')
 
 A = argv[1]
 if A == '-K':
-    import WumpusWorld.tests.kbank
+    import WumpusWorld.tests.kbank  # Initiates kbank test code
 if A == '--sim':
     from WumpusWorld.tests.sims import PPSim
     PPSim(argv[2])
